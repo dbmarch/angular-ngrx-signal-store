19ee0843a2054b9c74841ea6c0a45377fff4f80a
Dave March <dbmarch@gmail.com>
2026-01-10 22:46:20 -0500

    add signalMethod

--- practice/14. books-signal-method/src/app/pages/book-presenter/store/book-presenter.store.ts
+++ practice/14. books-signal-method/src/app/pages/book-presenter/store/book-presenter.store.ts
@@ -2,14 +2,15 @@ import { patchState, signalStore, withComputed, withMethods, withState } from '@
 import { initialBookPresenterSlice } from './book-presenter.slice';
 import { computed } from '@angular/core';
 import { BOOKS_COLLECTION } from '../../../data/books-collection';
+import { signalMethod } from '@ngrx/signals';
 export const BookPresenterStore = signalStore(
     withState(initialBookPresenterSlice), 
     withComputed(store => ({
         book: computed(() => BOOKS_COLLECTION[store.id()])
     })), 
     withMethods(store => ({
-        setBookId(id: number) {
+        setBookId:  signalMethod<number>(id => {
             patchState(store, {id});
-        }
+        })
     }))
 )
